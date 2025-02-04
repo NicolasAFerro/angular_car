62a4df675f0968c85627c40d0286c7d7e0ccb001
Nicolas Ferro <nicolasalexandrinoferro@gmail.com>
2025-02-04 12:34:10 -0300

    carregando lista fixa na tabela

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import{Car} from '../../Car';
+
+
 
 @Component({
   selector: 'app-cars',
   standalone: false,
-  
+
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
+  car: Car = {} as Car;
+  cars: Car[] = [
+    {
+      id: 1,
+      name: 'Fiesta',
+      automaker: 'Ford',
+      price: 19000,
+      ano: 2011,
+    },
+    {
+      id: 1,
+      name: 'Palio',
+      automaker: 'Fiat',
+      price: 12000,
+      ano: 2001,
+    },
+    {
+      id: 1,
+      name: 'Etios',
+      automaker: 'Toyota',
+      price: 40000,
+      ano: 2015,
+    },
+  ];
 
 }
