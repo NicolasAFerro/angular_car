488d6ce2837156b2ea8d3563eafdf603393c56ff
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2025-02-03 22:14:45 -0300

    criação do html

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Car } from '../../Car';
 
 @Component({
   selector: 'app-car',
   standalone: false,
-  
+
   templateUrl: './car.component.html',
-  styleUrl: './car.component.css'
+  styleUrl: './car.component.css',
 })
 export class CarComponent {
-
+  carro: Car = {} as Car;
 }
